f180a1b9de2e3017164883829da8f17145d12fe0
Kristina Brennan <96805863+thetinaest@users.noreply.github.com>
2022-08-21 18:27:49 -0500

    added transition out, new styling, added fontawesome icons

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Contact from './components/contact';
 import Footer from './components/footer';
 import Resume from './components/resume';
 
+//font awesome imports
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPeopleRoof, faBookBookmark, faCirclePlay, faCloudSun, faLaptopCode, faCalendarDay, faKey } from '@fortawesome/free-solid-svg-icons';
 
 const options = [
   {
@@ -32,43 +35,51 @@ const projects = [
     name: "Jessie's List",
     github: 'https://github.com/thetinaest/chores-app',
     live: 'https://nameless-inlet-86757.herokuapp.com/',
-    picture: require('./assets/choresList.png')
+    // picture: require('./assets/choresList.png')
+    picture: <FontAwesomeIcon icon={faPeopleRoof} />
   },
   {
     name: 'Book Bits',
     github: 'https://github.com/Mazattack1999/book-bits',
     live: 'https://book-bits-blog.herokuapp.com/',
-    picture: require('./assets/bookshelf.jpg')
+    // picture: require('./assets/bookshelf.jpg')
+    picture: <FontAwesomeIcon icon={faBookBookmark} />
+
   }, 
   {
     name: 'Tunalyze',
     github: 'https://github.com/thetinaest/Tunalyze',
     live: 'https://thetinaest.github.io/Tunalyze/',
-    picture: require('./assets/tunalyze.png')
+  //   picture: require('./assets/tunalyze.png')
+    picture: <FontAwesomeIcon icon={faCirclePlay} />
   }, 
   {
-    name: 'Weather Dashboard',
+    name: 'Weather App',
     github: 'https://github.com/thetinaest/weather-dashboard',
     live: 'https://thetinaest.github.io/weather-dashboard/',
-    picture: require('./assets/weather-dashboard.png')
+    // picture: require('./assets/weather-dashboard.png')
+    picture: <FontAwesomeIcon icon={faCloudSun} />
   },
   {
     name: 'Code Quiz',
     github: 'https://github.com/thetinaest/code-quiz',
     live: 'https://thetinaest.github.io/code-quiz/',
-    picture: require('./assets/coding-quiz.png')
+    //picture: require('./assets/coding-quiz.png')
+    picture: <FontAwesomeIcon icon={faLaptopCode} />
   },
   {
     name: 'Calendar',
     github: 'https://github.com/thetinaest/calendar',
     live: 'https://thetinaest.github.io/calendar/',
-    picture: require('./assets/calendar.png')
+    //picture: require('./assets/calendar.png')
+    picture: <FontAwesomeIcon icon={faCalendarDay} />
   },
   {
     name: 'Password Generator',
     github: 'https://github.com/thetinaest/password-generator',
     live: 'https://thetinaest.github.io/password-generator/',
-    picture: require('./assets/password-generator.png')
+    //picture: require('./assets/password-generator.png')
+  picture: <FontAwesomeIcon icon={faKey} />
   }
 ]
 
--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -10,10 +10,12 @@ function Projects(props) {
                 {projects.map(project => {
                         return(
                             <div className='project'>
-                                <img src={project.picture}></img>
-                                <h3>{project.name}</h3>
-                                <a href={project.github} target='_blank'>Github</a>
-                                <a href={project.live} target='_blank'>Deployed</a>
+                                <a href={project.live} target='_blank'>
+                                    {project.picture}
+                                    <h3>{project.name}</h3>
+                                </a>
+                                <a className='github' href={project.github} target='_blank'>Github</a>
+                                {/* <a href={project.live} target='_blank'>Deployed</a> */}
                             </div>
                         );
                     })}
